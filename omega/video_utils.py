c21e53da49a49961d3a5a21bf0153f2fe578707f
Michal Osinski <michal.rosiol@gmail.com>
2024-03-26 16:28:10 +0100

    modified:   omega/video_utils.py

--- omega/video_utils.py
+++ omega/video_utils.py
@@ -16,7 +16,7 @@ import logging
 from omega.constants import FIVE_MINUTES
 
 # Set up Redis connection
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0, ssl=True)
 
 def seconds_to_str(seconds):
     hours = seconds // 3600
